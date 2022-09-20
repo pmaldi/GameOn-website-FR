d60ca36f589c3a0ded9c862dbcfc5da192c0aa34
Patrice MALDI <patrice.maldi@gmail.com>
2022-09-20 11:33:56 +0200

    Ajouter confirmation quand envoi réussi

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -33,79 +33,139 @@ function closeModal() {
   modalbg.style.display = "none"
 }
 
+let formIsValid = false;
 
-// Fonction Validate pour le Formulaire
-function validate() {
+
+function first_validation() {
   const first = document.getElementById("first").value;
-  const last = document.getElementById("last").value;
-  const email = document.getElementById("email").value;
-  const birthdate = document.getElementById("birthdate").value;
-  const quantity = document.getElementById("quantity").value;
-  const town = document.getElementsByName("location");
-  const CCU = document.getElementById("checkbox1").checked;
-  const submitBtn = document.getElementsByClassName("btn-submit");
-
-  let isChecked = false; // permet d'avoir l'information si la ville est cochée ou pas.
-  let formIsValid = true;
-
+  const error = document.getElementById("first_error")
   if (first.length < 2) {
-    const error = document.getElementById("first_error")
     error.style.display = "block"
     error.innerText = "Veuillez entrer 2 caractères ou plus pour le champ du prénom."
 
-    formIsValid = false
+    return false
+  } else {
+    error.style.display = "none"
+    return true
   }
+}
 
+function last_validation() {
+  const last = document.getElementById("last").value;
+  const error = document.getElementById("last_error")
   if (last.length < 2) {
-    const error = document.getElementById("last_error")
     error.style.display = "block"
     error.innerText = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
 
-    formIsValid = false
+    return false
+  } else {
+    error.style.display = "none"
+    return true
   }
+}
+
+function email_validation() {
+  const email = document.getElementById("email").value;
+  const error = document.getElementById("email_error")
 
   if (!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-    const error = document.getElementById("email_error")
     error.style.display = "block"
     error.innerText = "Merci de renseigner un email valide";
 
-    formIsValid = false
+    return false
+  } else {
+    error.style.display = "none"
+    return true
   }
+}
+
+function birthdate_validation() {
+  const birthdate = document.getElementById("birthdate").value;
+  const error = document.getElementById("birthdate_error")
 
   if (birthdate.length != 10) {
-    const error = document.getElementById("birthdate_error")
     error.style.display = "block"
     error.innerText = "Vous devez entrer votre date de naissance.";
 
-    formIsValid = false
+    return false
+  } else {
+    error.style.display = "none"
+    return true
   }
+}
 
+function quantity_validation() {
+  const quantity = document.getElementById("quantity").value;
+  const error = document.getElementById("quantity_error")
   if (isNaN(parseInt(quantity))) {
-    const error = document.getElementById("quantity_error")
     error.style.display = "block"
     error.innerText = "Merci de renseigner un nombre de tournoi valide";
 
-    formIsValid = false
+    return false
+  } else {
+    error.style.display = "none"
+    return true
   }
+}
+
+function location_validation() {
+  const town = document.getElementsByName("location");
+  const error = document.getElementById("location_error")
+  let isChecked = false; // permet d'avoir l'information si la ville est cochée ou pas.
 
   town.forEach((ville) =>
     ville.checked ? isChecked = true : null
   )
 
   if (isChecked === false) {
-    const error = document.getElementById("location_error")
     error.style.display = "block"
     error.innerText = "Vous devez choisir une option.";
 
-    formIsValid = false
+    return false
+  } else {
+    error.style.display = "none"
+    return true
   }
+}
+
+function checkbox_validation() {
+  const CCU = document.getElementById("checkbox1").checked;
+  const error = document.getElementById("checkbox_error")
 
   if (CCU === false) {
-    const error = document.getElementById("checkbox_error")
     error.style.display = "block"
     error.innerText = "Vous devez vérifier que vous acceptez les termes et conditions.";
 
-    formIsValid = false
+    return false
+  } else {
+    error.style.display = "none"
+    return true
   }
+}
 
+
+// Fonction Validate pour le Formulaire
+function validate() {
+  first_validation();
+  last_validation();
+  email_validation();
+  birthdate_validation();
+  quantity_validation();
+  location_validation();
+  checkbox_validation();
+
+  if (first_validation() && last_validation() && email_validation() && birthdate_validation() && quantity_validation() && location_validation() && checkbox_validation()) {
+    cleanForm();
+  }
+}
+
+function cleanForm() {
+  document.getElementsByClassName("content")[0].style.height = "inherit"
+  const modalBody = document.getElementsByClassName("modal-body")[0]
+  modalBody.innerHTML = `<p class="confirmation-message">Merci ! Votre réservation a été reçue.</p><br><button class="btn-signup" onClick="closeModal();">Fermer</button>`
+  modalBody.style.height = "100%";
+  modalBody.style.display = "flex";
+  modalBody.style.alignItems = "center";
+  modalBody.style.flexDirection = "column";
+  modalBody.style.justifyContent = "center";
 }
